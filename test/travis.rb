aaa902594e6f916a423515f15f2beca2d05cfc90
JeroenPtrs <stagiair@Admins-MacBook-Pro.local>
2017-06-14 10:21:09 +0200

    More checks on file size

--- test/travis.rb
+++ test/travis.rb
@@ -6,4 +6,6 @@ puts "Regular compile worked successfully"
 
 result = `sass test/silent.scss build.silent.css --style compressed`
 raise result unless $?.to_i == 0
-puts File.size('build.silent.css')
+puts File.size?('build.silent.css')
+puts File.zero?('build.silent.css')
+puts File.empty?('build.silent.css')
