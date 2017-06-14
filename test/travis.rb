28627687334be288a09fae1f132328aa29a91be8
JeroenPtrs <stagiair@Admins-MacBook-Pro.local>
2017-06-14 10:06:52 +0200

    Print file size for debugging

--- test/travis.rb
+++ test/travis.rb
@@ -6,5 +6,6 @@ puts "Regular compile worked successfully"
 
 result = `sass test/silent.scss build.silent.css --style compressed`
 raise result unless $?.to_i == 0
-raise "When $use-silent-classes is set to true the module should not output any CSS" unless File.size('build.silent.css') == 0
+raise File.size('build.silent.css')
+raise "When $_silent is set to true the module should not output any CSS" unless File.size('build.silent.css') == 0
 puts "Silent sass compiled successfully"
